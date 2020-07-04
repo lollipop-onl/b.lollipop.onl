c08a24af55c9042a195813fff6f07e49f78f3200
tamuraryoya <tamuraryoya@team-lab.com>
2020-07-04 15:59:36 +0900

    🔧 Add Next.js config file

---
+++ next.config.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+module.exports = {
+  env: {
+    microCMS_API_KEY: process.env.microCMS_API_KEY
+  },
+  exportTrailingSlash: true,
+  exportPathMap: () => {
+    return {
+      '/404.html': { page: '/404' },
+    }
+  }
+};

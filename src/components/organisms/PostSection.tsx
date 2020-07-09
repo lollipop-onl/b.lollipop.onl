83c8dcd93635518c1e7c706387888dde8c205ee9
tamuraryoya <tamuraryoya@team-lab.com>
2020-07-09 19:26:19 +0900

    ✨ Add category link

--- src/components/organisms/PostSection.tsx
+++ src/components/organisms/PostSection.tsx
@@ -1,8 +1,11 @@
 import React, { FC } from 'react';
 import styled from 'styled-components';
+import Link from 'next/link';
 import { BlogPost, GyazoOEmbed } from '~/api/types';
 import { PostContent } from '~/components/atoms/PostContent';
 import { PostTitle } from '~/components/atoms/PostTitle';
+import * as C from '~/const';
+import { url } from '~/utils';
 
 const StyledPostTitle = styled(PostTitle)`
   margin-bottom: 24px;
@@ -25,6 +28,21 @@ export const PostSectionContent: FC<Props> = ({ post, thumbnailImage, contentHtm
   <>
     <div>
       <StyledPostTitle>{post.title}</StyledPostTitle>
+      <Link href={url(C.PAGES.BLOG_CATEGORY, { categoryId: post.category.id, page: 1 })}>
+        <a>
+          <span className="fas fa-folder" />
+          {post.category.name}
+        </a>
+      </Link>
+      <ol>
+        {post.tags.map((tag) => (
+          <li key={tag.id}>
+            <Link href={url(C.PAGES.BLOG_TAG, { tagId: tag.id, page: 1 })}>
+              <a>{tag.name}</a>
+            </Link>
+          </li>
+        ))}
+      </ol>
       <section>
         { thumbnailImage ? (
           <StyledPostThumbnail>
--- src/const/pages.ts
+++ src/const/pages.ts
@@ -1,5 +1,11 @@
 /** ページURL */
 export const PAGES = {
+  /** トップページ */
   TOP: '/',
+  /** ブログポスト */
   BLOG_POST: '/:contentId',
+  /** カテゴリポストリスト */
+  BLOG_CATEGORY: '/category/:categoryId/:page',
+  /** タグポストリスト */
+  BLOG_TAG: '/tag/:tagId/:page',
 };

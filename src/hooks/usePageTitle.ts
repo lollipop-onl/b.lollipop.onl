684317dc1d585de86607b9bc6cf2adf28bd81533
tamuraryoya <tamuraryoya@team-lab.com>
2020-07-10 18:55:15 +0900

    🔧 Update page title

--- src/hooks/usePageTitle.ts
+++ src/hooks/usePageTitle.ts
@@ -3,4 +3,4 @@
  * @param title タイトル
  * @param siteTitle サイトタイトル
  */
-export const usePageTitle = (title?: string, siteTitle = 'b.lollipop.onl'): string => (title ? `${title} | ${siteTitle}` : siteTitle);
+export const usePageTitle = (title?: string, siteTitle = 'Blog'): string => (title ? `${title} | ${siteTitle}` : siteTitle);
